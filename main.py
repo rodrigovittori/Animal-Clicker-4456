187f48abaa9399831bfc756f091a2a947fd688fa
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-05-28 18:49:53 -0300

    Actividad Nº 2 - "Aumentar la puntuacion"
    Agregamos lógica de colisión y aumento de puntuación

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 #pgzero
 
-""" [M8.L2] - Actividad Nº 1 - "Clicker Animal"
-    Objetivo: Crear ventana del juego
+""" PACK DE ASSETS: https://kenney.nl/assets/animal-pack-redux
 
-    PACK DE ASSETS: https://kenney.nl/assets/animal-pack-redux
-    FONDOS KODLAND: https://drive.google.com/drive/folders/1lXNvCF8q-IJ886xrCZmNt0ap1ElwGEU7
+    CARPETA FONDOS KODLAND: https://drive.google.com/drive/folders/1lXNvCF8q-IJ886xrCZmNt0ap1ElwGEU7
+    
+    Packs fondos Kenney: 
+    > https://kenney.nl/assets/background-elements
+    > https://kenney.nl/assets/background-elements-redux
     
     Link a este archivo (en GitHub): 
     Link al historial (en GitHub):    https://github.com/rodrigovittori/Animal-Clicker-4456/commits/main/
-
-    Paso 1º) #pgzero
-    Paso 2º) Ancho y alto de la ventana (WIDTH y HEIGHT)
-    Paso 3º) Título (TITLE) y FPS
-    Paso 4º) Crear Actor "Fondo" con modelo "background.jpg"
-    Paso 5º) Crear Actor "Animal" con modelo de Jirafa (giraffe.png) 
-    Paso 6º) Dibujar los elementos en pantalla ( draw() )
-    Paso 7º) Crear una variable "puntuacion" para registrar los clicks
-    Paso 8º) Agregar el método on_mouse_down(button, pos)
-    Paso 9º) Agregamos una condición en on_mouse_down() para contar los clicks
-    Paso 10) Agregamos un screen.draw.text para mostrar la puntuación
+    
+    --------------------------------------------------------------------------
+    
+    [M8.L2] - Actividad Nº 2 - "Aumentar la puntuacion"
+    Objetivo: Agregar condición de collide-point al Actor y animaciones
+    
+    Paso 1º) Creamos una variable que controle el multiplicador de click
+             > (cuántos puntos sumamos al hacer click sobre nuestro Actor)
+    Paso 2º) Modificamos on_mouse_down() para que SOLO aumente la puntuación cuando el click sea sobre el Personaje
+    Paso 3º) Agregamos una pequeña animación al hacer click sobre el personaje
 """
 
 WIDTH = 600  # Ancho de la ventana
@@ -31,7 +32,8 @@ fondo = Actor("background", size=(WIDTH, HEIGHT))
 animal = Actor("giraffe", (150, 250))
 
 #VARIABLES
-puntuacion = 0
+puntuacion = 0 # puntos actuales
+click_mult = 1 # multiplicador del valor por click
 
 def draw():
     fondo.draw()
@@ -42,4 +44,12 @@ def on_mouse_down(button, pos):
     global puntuacion
 
     if (button == mouse.LEFT):
-        puntuacion += 1
+        if(animal.collidepoint(pos)): # Si el click fue sobre el PJ:
+            puntuacion += click_mult
+
+            # Animación:
+            animal.y = 200
+            animate(animal, tween = "bounce_end", duration = 0.5, y = 250)
+            # https://pygame-zero.readthedocs.io/en/stable/builtins.html?highlight=animate#animate
+            
+            
